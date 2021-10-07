9ea70b1a647a40c99f3f3159151528de47ac39dc
Caleb <choonweiw@gmail.com>
2021-10-07 12:36:53 +0800

    Initialise files

---
+++ src/main/java/com/retrace/javasandbox/JavasandboxApplication.java
@@ -0,0 +1,13 @@
+package com.retrace.javasandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavasandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavasandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retrace/javasandbox/JavasandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retrace.javasandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavasandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
